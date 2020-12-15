ee0bbc2024a0ceb7eb6b112f7eece436d049a74d
Marcin <marcin05031997@gmail.com>
2020-12-15 23:58:53 +0100

    Add reservation

---
+++ src/client/reservation/ClientReservation.tsx
@@ -0,0 +1,138 @@
+import React from "react";
+import LoggedHeader from "../../shared/header/LoggedHeader";
+import {Button, ButtonGroup, Row} from "react-bootstrap";
+import SweetAlert from "react-bootstrap-sweetalert";
+import axios from "axios";
+import ResponseMessage from "../../ResponseMessage";
+import {RestaurantReservationDTO} from "../../restaurant/reservation/RestaurantReservationDTO";
+import {reservationStatuses} from "../../restaurant/reservation/RestaurantReservation";
+
+
+export default class ClientReservation extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {reservations: [], success: false, failure: false, message: '', filterToDisplay: ''};
+        axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+
+    }
+
+    componentDidMount() {
+        axios.get<RestaurantReservationDTO[]>(`/api/client/reservations`).then(result => {
+            const pendingReservations = result.data.filter((reservation: RestaurantReservationDTO) => reservation.status === 'PENDING');
+            this.setState({reservations: pendingReservations});
+        });
+        this.setState({filterToDisplay: 'PENDING'});
+    }
+
+    cancelReservation(id: number): void {
+        this.sendReportRequest(id, `/api/client/reservations/cancel/${id}`);
+    }
+
+    filterReservations(event: { preventDefault: () => void; }, filter: string): void {
+        event.preventDefault();
+        this.setState({filterToDisplay: filter});
+
+        axios.get<RestaurantReservationDTO[]>(`/api/client/reservations`).then(result => {
+            const pendingReservations = result.data.filter((reservation: RestaurantReservationDTO) => reservation.status === filter);
+            this.setState({reservations: pendingReservations});
+        });
+    }
+
+    sendReportRequest(id: number, url: string): void {
+        axios.put<ResponseMessage<string>>(url).then(result => {
+            const reservations = this.state.reservations.filter((reservation: RestaurantReservationDTO) => reservation.id !== id);
+
+            this.setState({reservations: reservations, success: true, message: result.data.message});
+        }).catch(reason => {
+            this.setState({failure: true, message: 'Error has occurred'});
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <LoggedHeader text={"Client"} link={"/client"}/>
+                <div>
+                    <Row className="justify-content-center align-self-center text-center">
+                        <ButtonGroup>
+                            {reservationStatuses.map((type: string) => {
+                                return (
+                                    <Button key={type} variant="secondary" name="filter"
+                                            onClick={e => this.filterReservations(e, type)}>{type}</Button>
+                                )
+                            })}
+                        </ButtonGroup>
+                    </Row>
+                    <Row className="justify-content-center align-self-center text-center">
+                        <span
+                            className="filter-text">Currently filtering by: {this.state.filterToDisplay}
+                        </span>
+                    </Row>
+
+                    <table className="table table-hover restaurantTable text-center">
+                        <thead>
+                        <tr>
+                            <th scope="col">User</th>
+                            <th scope="col">Restaurant</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Persons count</th>
+                            <th scope="col">Reservation date</th>
+                            <th scope="col">Reservation time</th>
+                            <th scope="col"/>
+                            <th scope="col"/>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.reservations.length > 0 ? this.state.reservations.map((reservation: RestaurantReservationDTO) => {
+                            return (
+                                <tr key={reservation.id}>
+                                    <td>
+                                        {reservation.clientUsername}
+                                    </td>
+                                    <td>
+                                        {reservation.restaurantName}
+                                    </td>
+                                    <td>
+                                        {reservation.status}
+                                    </td>
+                                    <td>
+                                        {reservation.personsCount}
+                                    </td>
+                                    <td>
+                                        {reservation.reservationDate}
+                                    </td>
+                                    <td>
+                                        {reservation.reservationTime}
+                                    </td>
+
+                                    {this.state.filterToDisplay === 'PENDING' &&
+                                    <td>
+                                        <button className="btn btn-danger"
+                                                onClick={() => this.cancelReservation(reservation.id)}>Cancel
+                                        </button>
+                                    </td>}
+
+                                </tr>
+                            )
+                        }) : <tr>
+                            <td colSpan={6}>There are no reservations</td>
+                        </tr>}
+                        </tbody>
+                    </table>
+                </div>
+
+                {this.state.success &&
+                <SweetAlert success title="Success" confirmBtnBsStyle={'info'} timeout={2000}
+                            onConfirm={() => this.setState({success: false})}>
+                    {this.state.message}
+                </SweetAlert>}
+                {this.state.failure &&
+                <SweetAlert error title="Failure" confirmBtnBsStyle={'info'} timeout={2000}
+                            onConfirm={() => this.setState({failure: false})}>
+                    {this.state.message}
+                </SweetAlert>}
+            </>
+        )
+    }
+}
--- src/shared/header/LoggedHeader.tsx
+++ src/shared/header/LoggedHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar } from "react-bootstrap"
+import {Nav, Navbar} from "react-bootstrap"
 import Logout from "../logout/Logout";
 import './LoggedHeader.less';
 
@@ -8,8 +8,15 @@ export default ({ text, link }: { text: string, link: string }) =>
         <Navbar.Brand href={ link } className="ml-auto"><h2 className='text-center'>Restauranty - { text }</h2>
         </Navbar.Brand>
         <Nav.Item className="ml-auto">
+            <div className="row">
+            {localStorage.getItem('role') === 'client' &&
+            <Nav.Link href='/client/reservations'>
+                <button className="btn btn-primary">Reservations</button>
+            </Nav.Link>
+            }
             <Nav.Link href='#'>
                 <Logout/>
             </Nav.Link>
+            </div>
         </Nav.Item>
     </Navbar>
